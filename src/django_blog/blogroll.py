fcf208caa33e50846f42e1eabc8f405d39215654
joe19940422 <joe19940422@gmail.com>
2021-12-26 14:27:14 +0100

    test

--- src/django_blog/blogroll.py
+++ src/django_blog/blogroll.py
@@ -4,7 +4,7 @@ __author__ = 'Fei'
 sites = [
     {"url": "https://apex.oracle.com/pls/apex/f?p=23635:1::::::", "name": "My personal Blog", "desc": "Based on Oracle Apex"},
     {"url": "https://www.linkedin.com/in/pengfei-qiao-a45a73178/", "name": "Linkin", "desc": "Welcome to contract me"},
-    {"url": "http://www.songluyi.com/", "name": "灯塔水母", "desc": "灯塔水母"},
+    {"url": "http://www.songluyi.com/", "name": "google", "desc": "灯塔水母"},
     {"url": "http://blog.topspeedsnail.com/", "name": "斗大的熊猫", "desc": "本博客专注于技术，Linux，编程，Python，C，Ubuntu、开源软件、Github等"},
     {"url": "https://www.urlteam.org/", "name": "URL-team", "desc": "URL-team"},
 ]
