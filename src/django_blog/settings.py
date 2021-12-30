ef87a674c26ec382133593566c82015dde61b6cb
joe19940422 <joe19940422@gmail.com>
2021-12-30 12:55:07 +0100

    test

--- src/django_blog/settings.py
+++ src/django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '85bus3!*+_3t1ks)&&o(u-liuhucshja!-44a1squ93=#+v+4r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pengfeiqiao.com','www.pengfeiqiao.com','ec2-15-236-211-95.eu-west-3.compute.amazonaws.com','35.180.248.118']
+ALLOWED_HOSTS = ['*']
 
 # HOST = "http://www.spiderpy.cn"
 
