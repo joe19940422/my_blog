29a668f4598615d8e98106eea65364b82e806738
joe19940422 <joe19940422@gmail.com>
2021-12-31 19:58:42 +0100

    test

--- src/blog/views.py
+++ src/blog/views.py
@@ -166,7 +166,7 @@ def Index(request):
 
 def bbc(request):
     newsapi = NewsApiClient(api_key="0aaf327d9eed48e2adb87d10f7946650")
-    topheadlines = newsapi.get_top_headlines(country='tw')
+    topheadlines = newsapi.get_top_headlines(country='us')
 
     articles = topheadlines['articles']
 
