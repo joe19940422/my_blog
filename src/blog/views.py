ff93aa5a66ef98074d6d406ac0f966a4715751e6
joe19940422 <joe19940422@gmail.com>
2022-01-01 20:00:45 +0100

    test

--- src/blog/views.py
+++ src/blog/views.py
@@ -144,7 +144,7 @@ def page_not_found_error(request, exception):
 def page_error(request):
     return render(request, "404.html", status=500)
 
-def china(request):
+def China(request):
     newsapi = NewsApiClient(api_key="0aaf327d9eed48e2adb87d10f7946650")
     topheadlines = newsapi.get_top_headlines(country='cn',language='zh')
 
@@ -185,4 +185,4 @@ def bbc(request):
 
     mylist = zip(news, desc,publishedAt, img)
     print(request)
-    return render(request, 'bbc.html', context={"mylist": mylist})
+    return render(request, 'china.html', context={"mylist": mylist})
