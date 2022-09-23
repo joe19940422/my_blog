ba59c1266eae65f4bd2b18028ad241f484b7ae74
joe19940422 <joe19940422@gmail.com>
2022-09-23 21:59:16 +0200

    test

--- src/django_blog/settings.py
+++ src/django_blog/settings.py
@@ -131,4 +131,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    # 'djangobower.finders.BowerFinder',
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
