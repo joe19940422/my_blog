3f84c39d8935f8e976650daf4f528d7d0259bfe6
joe19940422 <joe19940422@gmail.com>
2022-10-01 17:08:30 +0200

    test

--- src/django_blog/settings.py
+++ src/django_blog/settings.py
@@ -147,6 +147,7 @@ class IPAddressFilter(logging.Filter):
             if x_forwarded_for:
                 record.ip = x_forwarded_for.split(',')[0]
                 ip_location = geocoder.ip(f"{record.ip}")
+                record.location = ip_location.city
                 print(ip_location.city)
             else:
                 record.ip = record.request.META.get('REMOTE_ADDR')
