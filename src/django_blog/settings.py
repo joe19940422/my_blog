fdca0fb8ee7c255b5441f00a01071675c5d6de41
joe19940422 <joe19940422@gmail.com>
2022-10-14 21:29:03 +0200

    test

--- src/django_blog/settings.py
+++ src/django_blog/settings.py
@@ -149,6 +149,7 @@ class IPAddressFilter(logging.Filter):
         if hasattr(record, 'request'):
             x_forwarded_for = record.request.META.get('HTTP_X_FORWARDED_FOR')
             if x_forwarded_for:
+                record.data = record.request #
                 record.user = record.request.user
                 record.ip = x_forwarded_for.split(',')[0]
                 ip_location = geocoder.ip(f"{record.ip}")
@@ -157,6 +158,7 @@ class IPAddressFilter(logging.Filter):
                 record.city = ip_location.city
 
             else:
+                record.data = record.request #
                 record.user = record.request.user
                 record.ip = record.request.META.get('REMOTE_ADDR')
                 ip_location = geocoder.ip(f"{record.ip}")
@@ -225,7 +227,7 @@ LOGGING = {
     "version": 1,
     "formatters": {
         "request_formatter": {
-            "format": "%(asctime)s#%(name)s#%(user)s#%(ip)s#%(country)s#%(province)s#%(city)s#%(levelname)s#%(message)s",
+            "format": "%(asctime)s#%(name)s#%(user)s#%(ip)s#%(country)s#%(province)s#%(city)s#%(levelname)s#%(data)s",
             "datefmt": "%Y-%m-%d %H:%M:%S"
         },
     },
