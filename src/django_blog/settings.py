0e8002e4fc83d773a92c33424135cfb8fa8c9ba5
joe19940422 <joe19940422@gmail.com>
2022-10-16 13:20:48 +0200

    test

--- src/django_blog/settings.py
+++ src/django_blog/settings.py
@@ -148,6 +148,8 @@ class IPAddressFilter(logging.Filter):
     def filter(self, record):
         info = str(record).split(',')[-1]
         if 'HTTP_COOKIE' or '%s' not in info:
+            return False
+        else:
             if hasattr(record, 'request'):
                 x_forwarded_for = record.request.META.get('HTTP_X_FORWARDED_FOR')
                 if x_forwarded_for:
@@ -168,7 +170,7 @@ class IPAddressFilter(logging.Filter):
                     record.city = ip_location.city
                     record.record = info
 
-        return True
+            return True
 
 
 
