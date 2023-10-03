b3fb74ab7fd1c53946a0cbc625175f0e52dfe2e7
Qiao .P.F <pengfei.qiao@nielsen.om>
2023-10-03 14:16:19 +0200

    test

--- src/blog/views.py
+++ src/blog/views.py
@@ -10,7 +10,7 @@ from blog.models import Article, Comment, City, Visitor, Contact
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
-from django_ratelimit.decorators import ratelimit
+from ratelimit.decorators import ratelimit
 from newsapi import NewsApiClient
 from googletrans import Translator
 from django.core.mail import send_mail
