f51a792cc7cf82e841c6cdcdbd7d3cefec3ef68f
fei <pengfei.qiao@nielsen.com>
2025-02-14 20:46:01 +0100

    add more country

--- src/blog/vpn.py
+++ src/blog/vpn.py
@@ -200,13 +200,6 @@ info_vpn_account = {
 
 def create_vpn(country):
     vpn_ec2 = boto3.client('ec2', region_name=info_vpn_account[country]["region_name"])
-    if not get_running_instances(country):
-        html = generate_redirect_html(
-            message="There is a vpn server in current country you can just download from config button",
-            redirect_url="http://pengfeiqiao.com/blog/aws/",
-            countdown_seconds=10,
-        )
-        return HttpResponse(html)
 
     BlockDeviceMappings = [
         {
@@ -586,6 +579,13 @@ def aws_page(request):
 
         if 'start_us_vpn' in request.POST:
             country = 'us'
+            if not get_running_instances(country):
+                html = generate_redirect_html(
+                    message="There is a vpn server in current country you can just download from config button",
+                    redirect_url="http://pengfeiqiao.com/blog/aws/",
+                    countdown_seconds=10,
+                )
+                return HttpResponse(html)
             client_ip, _ = get_client_ip(request)
             if client_ip:
                 # Define a cache key based on the client's IP address
